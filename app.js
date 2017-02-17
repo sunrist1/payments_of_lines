f980743de30fc086f276231e223f0fa090f6cfb2
sunrist1 <294333575@qq.com>
2017-02-17 18:02:50 +0800

    b

--- app.js
+++ app.js
@@ -1,37 +1,32 @@
-var http = require('http');
-var url = "http://www.lianhanghao.com/"; 
 var cheerio = require('cheerio');
-var fs = require('fs');
+var Promise = require("bluebird")
+var fs = Promise.promisifyAll(require('fs'));
+var request = Promise.promisifyAll(require('request'))
 var path = require('path')
 var data = "";
+
+
+var url = "http://www.lianhanghao.com/"; 
+
 
 var banks = [],
     provinces = [];
 
-fs.readFile(__dirname + "/banks.json",{flag:'r+',encoding:'utf8'},function(err,data){
-  if(err){
+fs.readFileAsync(__dirname + "/banks.json",'utf8')
+  .then(function(error){
+    console.log(data)
+  })
+  .catch(function(err){
     console.error(err)
-    return;
-  }
-// console.log(data)
-  banks = JSON.parse(data)
+  });
 
-  console.log(banks[0])
-})
-
-var req = http.request(url,function(res){
-  res.setEncoding("utf8")
-
-  res.on('data',function(chunk){
-    data += chunk
+request.getAsync(url)
+  .then(function(data){
+    filter_bank(data)
   })
-
-  res.on("end",function(){
-    // filter_html(data)
-    // filter_bank(data)
-    // filter_province(data)
+  .catch(function(err){
+    console.error(err)
   })
-})
 
 function filter_html(html){
   var $ = cheerio.load(html)
@@ -43,57 +38,65 @@ function filter_html(html){
   })
 }
 
-// // 获取银行列表并保存为文件
-// function filter_bank(html){
-//   var $ = cheerio.load(html)
-//   var banks = $("#bank option");
-//   var banksArr = []
-//   banks.each(function(item){
-//     var summary = $(this)
-//     if(summary.text().indexOf("请选择")>0){
-//       return;
-//     }
-//     var bank = {
-//       name:summary.text(),
-//       code:summary.attr("value")
-//     }
-//     banksArr.push(bank)
-//   })
+// 获取银行列表并保存为文件
+function filter_bank(html){
+  var $ = cheerio.load(html)
+  var banks = $("#bank option");
+  var banksArr = []
+  banks.each(function(item){
+    var summary = $(this)
+    if(summary.text().indexOf("请选择")>0){
+      return;
+    }
+    var bank = {
+      name:summary.text(),
+      code:summary.attr("value")
+    }
+    banksArr.push(bank)
+  })
 
-//   var banksJson = JSON.stringify(banksArr)
-//   fs.writeFile(__dirname + '/banks.json',banksJson,function(err){
-//     if(err){
-//       console.error(err)
-//     }else{
-//       console.log('write banks json success')
-//     }
-//   })
-// }
-// // 获取省份列表并保存为文件
-// function filter_province(html){
-//   var $ = cheerio.load(html)
-//   var provinces = $("#province option");
-//   var provincesArr = []
-//   provinces.each(function(item){
-//     var summary = $(this)
-//     if(summary.text().indexOf("请选择")>0){
-//       return;
-//     }
-//     var province = {
-//       name:summary.text(),
-//       code:summary.attr("value")
-//     }
-//     provincesArr.push(filter_province)
-//   })
+  var banksJson = JSON.stringify(banksArr)
+  fs.writeFile(__dirname + '/banks.json',banksJson,function(err){
+    if(err){
+      console.error(err)
+    }else{
+      console.log('write banks json success')
+    }
+  })
+}
+// 获取省份列表并保存为文件
+function filter_area(html){
+  var $ = cheerio.load(html)
+  var provinces = $("#province option");
+  var provincesArr = []
+  provinces.each(function(item){
+    var summary = $(this)
+    if(summary.text().indexOf("请选择")>0){
+      return;
+    }
+    var province = {
+      name:summary.text(),
+      code:summary.attr("value")
+    }
+    provincesArr.push(province)
+  })
 
-//   var provincesJson = JSON.stringify(provincesArr)
-//   fs.writeFile(__dirname + '/provinces.json',provincesJson,function(err){
-//     if(err){
-//       console.error(err)
-//     }else{
-//       console.log('write provinces json success')
-//     }
-//   })
-// }
+  var provincesJson = JSON.stringify(provincesArr)
 
-req.end()
+  provincesArr.each(function(item){
+    http.request(url+"index.php/Index/Ajax?id="+item.code,function(res){
+      res.setEncoding("utf8")
+    })
+  })
+  /*fs.writeFile(__dirname + '/provinces.json',provincesJson,function(err){
+    if(err){
+      console.error(err)
+    }else{
+      console.log('write provinces json success')
+    }
+  })*/
+}
+
+function getCitys(provinces){
+
+}
